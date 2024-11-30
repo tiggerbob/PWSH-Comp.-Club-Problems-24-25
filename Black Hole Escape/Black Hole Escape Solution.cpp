abf0800077e03107fb6a50b307f72b10aea352ae
Annie <150103284+saladmence@users.noreply.github.com>
2024-11-29 18:30:53 -0600

    Add files via upload

---
+++ Black Hole Escape/Black Hole Escape Solution.cpp
@@ -0,0 +1,43 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int startFuel, startDist; cin >> startFuel >> startDist;
+
+	bool flag = false; char member;
+
+	for (int i = 0; i < 6; i++){
+		int fuel = startFuel; int dist = startDist;
+		cin >> member;
+		string actions; cin >> actions;
+		int j;
+		for (j = 0; j < actions.length(); j++){
+			if (fuel < 0 || dist < 0) {
+				break;
+			}
+			else {
+				if (actions[j] == 'r'){
+					fuel += 3; dist -= 4;
+				} else {
+					int val = actions[j] - '0';
+					fuel -= 2 * val; dist += val;
+				}
+			}
+		}
+
+		if (j == actions.length()){
+			flag = true;
+			break;
+		}
+	}
+
+	if (flag == true){
+		cout << member;
+	} else {
+		cout << "none";
+	}
+	
+}
