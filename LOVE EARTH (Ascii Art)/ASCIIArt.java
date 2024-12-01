d9fdd936863c0469def3527d40080feda1aaed1d
ShootTheBucket756 <mxl75024@gmail.com>
2024-11-30 21:03:53 -0600

    Add files via upload

---
+++ LOVE EARTH (Ascii Art)/ASCIIArt.java
@@ -0,0 +1,17 @@
+public class ASCIIArt
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("\\     ,-:`I\\;',`'-,");    
+        System.out.println("\\   .'-;_,;  ':-;_,'.");      
+        System.out.println("\\  /;   '/LOVE,  _`.-\\");
+        System.out.println("\\ |-'`.-(`-----/` `-\\`|");
+        System.out.println("\\ |:.  `\\`-.   \\_   / |");
+        System.out.println("\\ |-----(---`,--.`\\ ;'|");
+        System.out.println("\\  \\     | .'     `-'/");
+        System.out.println("\\   `.   ;/ EARTH  .'");
+        System.out.println("\\     `'-._____.--/");
+        System.out.println("\\\\|-------------------|");
+
+    }
+}
---
+++ LOVE EARTH (Ascii Art)/ASCIIArt.md
@@ -0,0 +1,11 @@
+# **LOVE EARTH**
+Billy is a kindergardener that love space. His favorite planet is Earth becuase all produces his favorite product: sugar. Can you help him draw Earth out to to help him demostrate his love for sugar and the Earth? Billy request Earth looks like this: 
+
+## Input Format
+None
+
+## Constriants 
+None
+
+## Output Format
+<img src="Ascii Art Image.png" alt="Your Screwed :)">
