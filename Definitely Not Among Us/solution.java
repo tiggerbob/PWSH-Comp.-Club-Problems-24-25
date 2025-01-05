235c21bac3cc6c603fd199c1c668347b2018fde5
Natalie 3 <natalie.nenx@gmail.com>
2025-01-05 16:49:15 -0600

    Create solution.java

---
+++ Definitely Not Among Us/solution.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String input = getInput();
+        Scanner scan = new Scanner(input);
+
+        int n = scan.nextInt(); // num testcases
+
+        for (int t = 0; t < n; t++) {
+            // testcase input
+            int numImpostors = scan.nextInt();
+            int numCrewmates = scan.nextInt();
+
+            int[] numConfirmers = new int[numCrewmates]; // number who confirmed crewmate idx
+            boolean[] isInnocent = new boolean[numCrewmates]; // is crewmate idx innocent?
+
+            ArrayList<ArrayList<Integer>> confirmations = new ArrayList<>();
+            for (int i = 0; i < numCrewmates; i++) {
+                confirmations.add(new ArrayList<>());
+            }
+
+            while (scan.hasNextInt()) {
+                int confirmer = scan.nextInt()-1;
+                int confirmee = scan.nextInt()-1;
+                confirmations.get(confirmer).add(confirmee);
+                numConfirmers[confirmee]++;
+            }
+
+            // logic
+            Queue<Integer> newlyInnocent = new LinkedList<>(); // we have work to do
+            for (int i = 0; i < numCrewmates; i++) {
+                if (numConfirmers[i] > numImpostors) {
+                    newlyInnocent.add(i);
+                    isInnocent[i] = true;
+                }
+            }
+
+            while (!newlyInnocent.isEmpty()) {
+                int confirmer = newlyInnocent.poll();
+                for (Integer crewmate : confirmations.get(confirmer)) {
+                    if (!isInnocent[crewmate]) { // not previously marked inno
+                        newlyInnocent.add(crewmate);
+                        isInnocent[crewmate] = true;
+                    }
+                }
+            }
+
+            int numPrinted = 0;
+            for (int i = 0; i < numCrewmates; i++) {
+                if (!isInnocent[i]) {
+                    System.out.println(i+1); // solution
+                    numPrinted++;
+                    if (numPrinted == numImpostors) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
