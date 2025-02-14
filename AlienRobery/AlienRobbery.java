80a349681fe2db43f2eb5dcc9341c65ed462ee50
ShootTheBucket756 <mxl75024@gmail.com>
2025-02-13 22:32:44 -0600

    Update AlienRobbery.java

--- AlienRobery/AlienRobbery.java
+++ AlienRobery/AlienRobbery.java
@@ -1,71 +1,92 @@
-import java.math.BigInteger;
+package AlienRobery;
+
 import java.io.*;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
-
-public class AlienCurrencyExchange 
+public class AlienRobbery 
 {
     public static void main(String[] args) throws IOException
-    {   
+    {
         //Timing
         long startTime = System.nanoTime();
         
-        //Starting Nums
-        long curEx = 7552701074431167L;
+        //IO Files
+        BufferedReader re = new BufferedReader( new FileReader("AlienRobery\\Input.txt") );
+        PrintWriter output = new PrintWriter("AlienRobery\\Output.txt");
         
-        //Getting Ghoa value
-        BufferedReader re = new BufferedReader( new FileReader("AlienCurrencyExchange\\Input.txt") );
-        PrintWriter output = new PrintWriter("AlienCurrencyExchange\\Output.txt");
-        long x = Long.valueOf( re.readLine() );
-        int sign = (x < 0)? -1 : 1;
-        x = Math.abs(x);
-        
+        //Taking in Balance
+        BigDecimal bal = new BigDecimal( re.readLine() );
 
-        //Getting Highest Degreee
-        long temp = x;
-        int deg = 0;
-        while( temp > 0 )
+        //Taking in Robbery
+        int t = Integer.parseInt( re.readLine() );
+        
+        for(int i = 0; i < t; i++)
         {
-            deg++;
-            temp /= 10;
+            //Taking Line Input
+            int robPercent = 0;
+            String[] arr = re.readLine().split("[\\s:]");
+            /* [Num People, Hour, Minute, PM or AM] */
+
+            //Processing Input 
+            //n
+            robPercent += Integer.parseInt(arr[0]) * 10;
+            /*
+             * Times
+             * t <= 8:00 AM -> 480 Min
+             * t >= 8:00 PM -> 1200 Min
+             */
+            int timeMin;
+            if( Integer.parseInt(arr[1]) != 12 )
+                timeMin = Integer.parseInt(arr[1]) * 60 + Integer.parseInt(arr[2]) + ((arr[3].equals("PM"))? 720 : 0);
+            else
+                timeMin = Integer.parseInt(arr[1]) * 60 + Integer.parseInt(arr[2]) + ((arr[3].equals("AM"))? 720 : 0);
+            if( timeMin <= 480 || timeMin >= 1200 ) robPercent += 5;
+
+            //If over 100%, reduce to one -> can not have negative balance
+            robPercent = (robPercent > 100)? 100 : robPercent;
+
+            //Calculating Amount Robbed
+            BigDecimal rob = new BigDecimal(bal.toString());
+            rob = rob.divide(new BigDecimal(100));
+            rob = rob.multiply(new BigDecimal(robPercent));
+            bal = bal.subtract(rob);
+
+            //Outputing Amount Robbed
+            output.println( round3Dig(rob) );
         }
 
-        //Calculating Rate
-        BigInteger exRate = new BigInteger( String.valueOf(curEx) );
-        BigInteger sum = new BigInteger("0");
-        while(x > 0)
-        {
-            //Calculating the individual number position
-            long tenPow = (int) Math.pow(10,deg-1);
-            BigInteger placeVal = new BigInteger( String.valueOf(x / tenPow * tenPow) );
-
-            //Calculating exchange at current place value
-            BigInteger placeCurEx = exRate.multiply(placeVal);
-
-            //Adding to next currency exchange
-            sum = sum.add(placeCurEx);
-
-            //recalculating exchange rate
-            exRate = exRate.multiply( new BigInteger("4") );
-            exRate = exRate.divide( new BigInteger("5") );
-            
-            //reducing x and highest degree
-            x -= placeVal.longValue();
-            deg--;
-        }
-
-        //Outputing Result
-        if(sign < 0)
-            output.print( "-" + sum.toString() );
-        else
-            output.print( sum.toString() );
+        //Outputing Remaining Balanced
+        output.println( round3Dig(bal) );
 
         //Timing
         long endTime = System.nanoTime();
         System.out.println("Time is: " + (endTime-startTime) /(double) 1000000000 + " seconds");
-
-        //Closing and Saving IO Files
-        output.close();
+        
+        //Closing IO Files
         re.close();
+        output.close();
+    }
+
+    public static BigDecimal round3Dig(BigDecimal old)
+    {
+        //Creating the return big decimal
+        //Adds one to ensure correct count of digits
+        BigDecimal ret = old.add(new BigDecimal(1)).stripTrailingZeros();
+        
+        //Getting Where decimal points is
+        int dot = ret.toString().indexOf(".");
+        int numDecs = ret.toString().length() - dot - 1;
+
+        //Formating to make sure include exactly 3 decimal spots
+        if( dot == -1 ) //Missing all trailing zeros
+            return ret.divide(new BigDecimal(1000)).multiply(new BigDecimal(1000)).subtract(new BigDecimal(1));
+        if( numDecs == 2 )  //missing two trailing zeros 
+            return ret.divide(new BigDecimal(10)).multiply(new BigDecimal(10)).subtract(new BigDecimal(1));
+        if( numDecs == 1 ) //missing one trailing zero
+            return ret.divide(new BigDecimal(100)).multiply(new BigDecimal(100)).subtract(new BigDecimal(1));
+        //3 or more deciamal places
+        else return ret.round(new MathContext(ret.precision() - (numDecs - 3)) ).subtract(new BigDecimal(1));
     }
     
 }
