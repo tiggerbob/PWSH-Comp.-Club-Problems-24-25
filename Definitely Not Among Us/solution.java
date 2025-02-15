6b0141c04cac27bea0f2cac06756ab9e9121504d
Natalie 3 <natalie.nenx@gmail.com>
2025-02-14 19:52:06 -0600

    Update solution.java
    copy pasted this from the hackkerank. this solution should work? it works for me

--- Definitely Not Among Us/solution.java
+++ Definitely Not Among Us/solution.java
@@ -2,8 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String input = getInput();
-        Scanner scan = new Scanner(input);
+        Scanner scan = new Scanner(System.in);
 
         int n = scan.nextInt(); // num testcases
 
